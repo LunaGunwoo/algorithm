2f400c85f13f4f7f67ce0f7b15b383d0ae601ae6
문건우 <104514539+LunaGunwoo@users.noreply.github.com>
2026-03-25 11:40:12 +0900

    [Silver II] Title: 욱제가 풀어야 하는 문제, Time: 40 ms, Memory: 8620 KB -BaekjoonHub

---
+++ 백준/Silver/18249. 욱제가 풀어야 하는 문제/욱제가 풀어야 하는 문제.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dp[191'230];
+
+int solve(int num) {
+  if (num <= 1)
+    return 1;
+  else if (dp[num] == -1)
+    return dp[num] = (solve(num - 1) + solve(num - 2)) % 1'000'000'007;
+  return dp[num];
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  fill(dp, dp + 191'230, -1);
+  dp[0] = dp[1] = 1;
+
+  int T;
+  cin >> T;
+  while (T--) {
+    int N;
+    cin >> N;
+    cout << solve(N) << '\n';
+  }
+  return 0;
+}
